1b0618a0af66efd6fb2804d1dbf97f23f2ab02aa
koomekevo <koome.kevo@gurucrafts.net>
2022-08-07 12:29:27 +0300

    Alter project structure into components and pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
+
+const Container = styled.div`
+  /* Surface */
+  
+  position: absolute;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+  
+  /* --c-white */
+  
+  background: #FFFFFF;
+  `
 export default class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+      <Container>
+        navbar
+      </Container>
     )
   }
 }
