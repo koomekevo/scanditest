eec3c65884561043c8a447d9b8700b969001e268
koomekevo <koome.kevo@gurucrafts.net>
2022-08-07 22:21:52 +0300

    Add KIDS button

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -283,6 +283,132 @@ const ButtonMenPaddingRight = styled.div`
   flex-grow: 0;
 `;
 
+// Start of ButtonKids
+const ButtonKids = styled.div`
+  /* _Header / Elements / Desktop / Buttons */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  padding: 0px;
+
+  width: 68px;
+  height: 56px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+`;
+
+const ButtonKidsPaddingLeft = styled.div`
+  /* Padding left */
+
+  width: 16px;
+  height: 2px;
+
+  /* --c-primary */
+
+  background: #5ece7b;
+  opacity: 0;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const ButtonKidsLabelLayout = styled.div`
+  /* Label */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  gap: 30px;
+
+  width: 36px;
+  height: 52px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const ButtonKidsLabel = styled.div`
+  /* Label */
+
+  width: 36px;
+  height: 20px;
+
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 120%;
+  /* identical to box height, or 19px */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-transform: uppercase;
+
+  /* --c-text */
+
+  color: #1d1f22;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const ButtonKidsBorder = styled.div`
+  /* Border */
+
+  width: 36px;
+  height: 2px;
+
+  /* --c-primary */
+
+  background: #5ece7b;
+  opacity: 0;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const ButtonKidsPaddingRight = styled.div`
+  /* Padding right */
+
+  width: 16px;
+  height: 2px;
+
+  /* --c-primary */
+
+  background: #5ece7b;
+  opacity: 0;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+`;
+
 export default class Navigation extends Component {
   render() {
     return (
@@ -308,6 +434,14 @@ export default class Navigation extends Component {
             </ButtonMenLabelLayout>
             <ButtonMenPaddingRight></ButtonMenPaddingRight>
           </ButtonMen>
+          <ButtonKids>
+            <ButtonKidsPaddingLeft></ButtonKidsPaddingLeft>
+            <ButtonKidsLabelLayout>
+              <ButtonKidsLabel>KIDS</ButtonKidsLabel>
+              <ButtonKidsBorder></ButtonKidsBorder>
+            </ButtonKidsLabelLayout>
+            <ButtonKidsPaddingRight></ButtonKidsPaddingRight>
+          </ButtonKids>
         </HeaderNavigation>
       </NavigationLayout>
     );
