2bec09f0241a57d6998ec71f1f6fcbc32e5e32d5
koomekevo <koome.kevo@gurucrafts.net>
2022-08-15 12:40:27 +0300

    Modify ProductCardBL.jsx to ProductCardBottomLeft.jsx

--- src/components/Body/ProductCardBottomLeft.jsx
+++ src/components/Body/ProductCardBottomLeft.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // eslint-disable-next-line no-unused-vars
 import styled from 'styled-components';
 
-export default class ProductCardBL extends Component {
+export default class ProductCardBottomLeft extends Component {
   render() {
     return (
       <div />
