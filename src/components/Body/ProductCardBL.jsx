9c60f62c92b5ee8cd36eb891f64969e378a32d75
koomekevo <koome.kevo@gurucrafts.net>
2022-08-15 12:36:56 +0300

    Add ProductCardBL.jsx

---
+++ src/components/Body/ProductCardBL.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components';
+
+export default class ProductCardBL extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
