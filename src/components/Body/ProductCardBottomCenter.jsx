3435f6f9c09f22a14088e71d3a68c0250480dcc6
koomekevo <koome.kevo@gurucrafts.net>
2022-08-19 13:02:22 +0300

    Add ProductCardBottomCenter.jsx component

---
+++ src/components/Body/ProductCardBottomCenter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProductCardBottomCenter extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default ProductCardBottomCenter;
