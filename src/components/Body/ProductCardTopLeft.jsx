aa489afda19c5c777ee35d216b32e89bc023ffbb
koomekevo <koome.kevo@gurucrafts.net>
2022-08-20 10:22:59 +0300

    Add ProductCardTopLeft.jsx and associated components

---
+++ src/components/Body/ProductCardTopLeft.jsx
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import ProductD from '../../assets/images/body/ProductD.png';
+
+const ProductCardTopLeftLayout = styled.div`
+  /* Product Card - Top Left Desktop */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: absolute;
+  width: 386px;
+  height: 444px;
+  left: calc(50% - 386px / 2 - 427px);
+  bottom: 738px;
+`;
+
+const ProductCardTopLeftElements = styled.div`
+  /* Product Card / Elements / Desktop */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 16px;
+
+  width: 386px;
+  height: 444px;
+
+  /* --c-white */
+
+  background: #ffffff;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const ProductCardTopLeftImageLayout = styled.div`
+  /* Image */
+
+  width: 354px;
+  height: 330px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const ProductCardTopLeftImageFrameLayout = styled.img.attrs({
+  src: `${ProductD}`,
+})`
+  /* Image */
+
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  bottom: 0px;
+`;
+
+const ProductCardTopLeftImageSpacerBase = styled.div`
+  /* _layout / --spacer-base */
+
+  width: 24px;
+  height: 24px;
+
+  opacity: 0;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const ProductCardTopLeftContent = styled.div`
+  /* Content */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  width: 354px;
+  height: 58px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const ProductCardTopLeftContentTitle = styled.div`
+  /* Title */
+
+  width: 354px;
+  height: 29px;
+
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 160%;
+  /* identical to box height, or 29px */
+
+  display: flex;
+  align-items: center;
+
+  /* --c-black */
+
+  color: #1d1f22;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const ProductCardTopLeftContentPrice = styled.div`
+  /* Price */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+
+  width: 58px;
+  height: 29px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+export default class ProductCardTopLeft extends Component {
+  render() {
+    return (
+      <ProductCardTopLeftLayout>
+        <ProductCardTopLeftElements>
+          <ProductCardTopLeftImageLayout>
+            <ProductCardTopLeftImageFrameLayout />
+          </ProductCardTopLeftImageLayout>
+          <ProductCardTopLeftImageSpacerBase />
+          <ProductCardTopLeftContent>
+            <ProductCardTopLeftContentTitle>
+              Apollo Running Short
+            </ProductCardTopLeftContentTitle>
+            <ProductCardTopLeftContentPrice>
+              $50.00
+            </ProductCardTopLeftContentPrice>
+          </ProductCardTopLeftContent>
+        </ProductCardTopLeftElements>
+      </ProductCardTopLeftLayout>
+    );
+  }
+}
