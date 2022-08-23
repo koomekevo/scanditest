805c0cb29b468ce249201b768e680a012ea7fd0b
koomekevo <koome.kevo@gurucrafts.net>
2022-08-23 23:10:53 +0300

    Add ColorFrameOneSeven component

--- src/components/Body/ProductDescriptionBody/ProductDescription.jsx
+++ src/components/Body/ProductDescriptionBody/ProductDescription.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import ProductD from '../../../assets/images/body/ProductD.png';
+import Colorgrey from '../../../assets/images/body/Colorgrey.svg';
+import Colorblack from '../../../assets/images/body/Colorblack.svg';
+import Colorgreen from '../../../assets/images/body/Colorgreen.svg';
 
 const ProductBottomThumbnail = styled.img.attrs({
   src: `${ProductD}`,
@@ -17,26 +20,90 @@ const ProductBottomThumbnail = styled.img.attrs({
 const ProductCenterThumbnail = styled.img.attrs({
   src: `${ProductD}`,
 })`
-    /* Image */
-  
-    position: absolute;
-    width: 79px;
-    height: 80px;
-    left: 100px;
-    top: 280px;
-  `;
+  /* Image */
+
+  position: absolute;
+  width: 79px;
+  height: 80px;
+  left: 100px;
+  top: 280px;
+`;
 
 const ProductTopThumbnail = styled.img.attrs({
   src: `${ProductD}`,
 })`
-    /* Image */
-  
-    position: absolute;
-    width: 79px;
-    height: 80px;
-    left: 100px;
-    top: 160px;
-  `;
+  /* Image */
+
+  position: absolute;
+  width: 79px;
+  height: 80px;
+  left: 100px;
+  top: 160px;
+`;
+
+const ColorFrameOneSeven = styled.div`
+  /* Frame 17 */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+
+  position: absolute;
+  width: 116px;
+  height: 36px;
+  left: 929px;
+  top: 394px;
+`;
+
+const GroupSixThreeOne = styled.img.attrs({
+  src: `${Colorgrey}`,
+})`
+  /* Group 631 */
+
+  width: 36px;
+  height: 36px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const GroupFourZeroSeven = styled.img.attrs({
+  src: `${Colorblack}`,
+})`
+  /* Group 407 */
+
+  width: 32px;
+  height: 32px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const GroupFourZeroEight = styled.img.attrs({
+  src: `${Colorgreen}`,
+})`
+  /* Group 408 */
+
+  width: 32px;
+  height: 32px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+`;
 
 class ProductDescription extends Component {
   render() {
@@ -45,6 +112,11 @@ class ProductDescription extends Component {
         <ProductBottomThumbnail />
         <ProductCenterThumbnail />
         <ProductTopThumbnail />
+        <ColorFrameOneSeven>
+          <GroupSixThreeOne />
+          <GroupFourZeroSeven />
+          <GroupFourZeroEight />
+        </ColorFrameOneSeven>
       </>
     );
   }
