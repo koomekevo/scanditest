9d7e4c0837b2147d9b76e5cccdb8e4f67f0db20e
koomekevo <koome.kevo@gurucrafts.net>
2022-08-23 16:14:56 +0300

    Add ProductDescriptionLayout component

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header/Header';
+
+const ProductDescriptionLayout = styled.div`
+  /* PDP */
+
+  position: relative;
+  width: 1440px;
+  height: 933px;
+`;
 
 export default class ProductDescription extends Component {
   render() {
     return (
-      <Header />
+      <ProductDescriptionLayout>
+        <Header />
+      </ProductDescriptionLayout>
     );
   }
 }
