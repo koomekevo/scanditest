87dc3a00a7cdebab657f9b2d1f5bab08403b6375
koomekevo <koome.kevo@gurucrafts.net>
2022-08-24 15:03:32 +0300

    Add Tax and Quantity Components

--- src/components/Body/CartPageBody/CartBody.jsx
+++ src/components/Body/CartPageBody/CartBody.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const FrameThreeTwo = styled.div`
   /* Frame 32 */
@@ -40,11 +40,81 @@ const FrameThreeOne = styled.div`
   flex-grow: 0;
 `;
 
+const FrameTwoNine = styled.div`
+  /* Frame 29 */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+
+  width: 184px;
+  height: 64px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const Tax = styled.div`
+  /* Tax 21%: $42.00 */
+
+  width: 184px;
+  height: 28px;
+
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  /* identical to box height, or 117% */
+
+  color: #1d1f22;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const Quantity = styled.div`
+  /* Quantity: 3 */
+
+  width: 118px;
+  height: 28px;
+
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  /* identical to box height, or 117% */
+
+  color: #1d1f22;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
 export default class CartBody extends Component {
   render() {
     return (
       <FrameThreeTwo>
-        <FrameThreeOne />
+        <FrameThreeOne>
+          <FrameTwoNine>
+            <Tax>Tax 21%: $42.00</Tax>
+            <Quantity>Quantity: 3</Quantity>
+          </FrameTwoNine>
+        </FrameThreeOne>
       </FrameThreeTwo>
     );
   }
