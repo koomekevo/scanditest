794ca6775e96f62b7b5631dac1d694951df6c60f
koomekevo <koome.kevo@gurucrafts.net>
2022-08-24 14:45:57 +0300

    Add FrameThreeOne component

--- src/components/Body/CartPageBody/CartBody.jsx
+++ src/components/Body/CartPageBody/CartBody.jsx
@@ -19,8 +19,33 @@ const FrameThreeTwo = styled.div`
   top: 962px;
 `;
 
+const FrameThreeOne = styled.div`
+  /* Frame 31 */
+
+  /* Auto layout */
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+
+  width: 198px;
+  height: 100px;
+
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
 export default class CartBody extends Component {
   render() {
-    return <FrameThreeTwo />;
+    return (
+      <FrameThreeTwo>
+        <FrameThreeOne />
+      </FrameThreeTwo>
+    );
   }
 }
