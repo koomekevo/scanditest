b644685459dbe5bba344a2289872af70a3f9723f
koomekevo <koome.kevo@gurucrafts.net>
2023-02-07 17:44:17 +0300

    Add ApolloClient in src/index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+/* eslint-disable */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
